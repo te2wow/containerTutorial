2431d8b0ffac393df118d98296dc23685c6f829b
teppei0717 <teppei0717@pepabo.com>
2024-06-20 12:18:35 +0900

    こっちのテストはなくていいかな。

--- service_b/test/controllers/random_string_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class RandomStringControllerTest < ActionDispatch::IntegrationTest
-    test 'should get show' do
-        get random_string_show_url
-      assert_response :success
-    end
-end
