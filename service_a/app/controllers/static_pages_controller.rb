a12786d0bb976533186b4329e5977e0b0a86ec80
teppei0717 <teppei0717@pepabo.com>
2024-06-27 10:15:43 +0900

    get先を指定しました。

--- service_a/app/controllers/static_pages_controller.rb
+++ service_a/app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
 
-    response = HTTParty.get('http://service_b:3000/random_string') # サービスBのURLを指定
+    response = HTTParty.get('http://service-b.tetsuwo.svc.cluster.local:8082/random_string') # サービスBのURLを指定
     @random_string = response.parsed_response['random_string']
   rescue StandardError => e
     @random_string = "Error: #{e.message}"
