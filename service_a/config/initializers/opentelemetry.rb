2c0fa86a4a6910cbab64a60b2d43490fd3ba4eca
teppei0717 <teppei0717@pepabo.com>
2024-07-02 10:48:44 +0900

    環境変数で渡すようにした

--- service_a/config/initializers/opentelemetry.rb
+++ service_a/config/initializers/opentelemetry.rb
@@ -1,24 +1,6 @@
-# config/initializers/opentelemetry.rb
-
 require 'opentelemetry/sdk'
-require 'opentelemetry/exporter/otlp'
-
-# OpenTelemetryの初期化
+require 'opentelemetry/instrumentation/all'
 OpenTelemetry::SDK.configure do |c|
-  c.use_all() # すべての利用可能なインストルメントを使用
-
-  c.add_span_processor(
-    OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
-      OpenTelemetry::Exporter::OTLP::Exporter.new(
-        endpoint: 'http://otel-collector-opentelemetry-collector.monitoring.svc.cluster.local:4317', # OpenTelemetry Collectorのエンドポイント
-      )
-    )
-  )
-end
-
-tracer = OpenTelemetry.tracer_provider.tracer('example_tracer')
-
-tracer.in_span('doing_work') do |span|
-  # ここにトレースするコードを記述
-  puts 'Doing some work...'
+  c.service_name = 'service-a'
+  c.use_all() # enables all instrumentation!
 end
