8ef7eedee06e286f1be0c83d0c2f407d3060ee51
teppei0717 <teppei0717@pepabo.com>
2024-07-02 15:44:30 +0900

    名前の変更

--- service_a/config/initializers/opentelemetry.rb
+++ service_a/config/initializers/opentelemetry.rb
@@ -2,6 +2,6 @@
 require 'opentelemetry/sdk'
 require 'opentelemetry/instrumentation/all'
 OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'service-a'
+  c.service_name = 'tetsuwo-service-a'
   c.use_all() # enables all instrumentation!
 end
--- service_b/config/initializers/opentelemetry.rb
+++ service_b/config/initializers/opentelemetry.rb
@@ -1,6 +1,6 @@
 require 'opentelemetry/sdk'
 require 'opentelemetry/instrumentation/all'
 OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'service-b'
+  c.service_name = 'tetsuwo-service-b'
   c.use_all() # enables all instrumentation!
 end
