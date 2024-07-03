0198fabfb90feeffdae59f0ed18b99bbe7df6363
teppei0717 <teppei0717@pepabo.com>
2024-07-03 10:48:07 +0900

    logに出力するようにした

--- service_b/app/controllers/random_string_controller.rb
+++ service_b/app/controllers/random_string_controller.rb
@@ -1,5 +1,10 @@
 class RandomStringController < ApplicationController
     def show
-        render json: { random_string: $random_string }
+      random_string = $random_string
+  
+      # レスポンスを返す前にログに記録
+      Rails.logger.info("Generated random string: #{random_string}")
+  
+      render json: { random_string: random_string }
     end
-end
+  end
