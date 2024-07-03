58f7e97e7ec0ae8b1b16b94bb760a3a921492387
teppei0717 <teppei0717@pepabo.com>
2024-07-03 13:01:04 +0900

    壊した

--- service_b/config/environments/development.rb
+++ service_b/config/environments/development.rb
@@ -53,6 +53,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.hosts << 'service-b.tetsuwo.svc.cluster.local'
-  config.hosts << 'service-b.tetsuwo.svc.cluster.local/random_string'
+   config.hosts << 'service-b.tetsuwo.svc.cluster.local'
+  # config.hosts << 'service-b.tetsuwo.svc.cluster.local/random_string'
 end
